faaad360907468c798eb1403b4bc9804a49b2559
Muad96 <38084566+Muad96@users.noreply.github.com>
2020-02-21 07:46:43 +0300

    Update Doc file

--- kader/config/docs.py
+++ kader/config/docs.py
@@ -4,7 +4,7 @@ Configuration for docs
 
 # source_link = "https://github.com/Muad96/kader2"
 # docs_base_url = "https://Muad96.github.io/kader"
-# headline = "HRMS APP"
+# headline = "HRMS"
 # sub_heading = "Yes, you got that right the first time, everything"
 
 def get_context(context):
